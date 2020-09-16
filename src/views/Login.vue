a4f885e99d85f1ae3a56314059dc35f02ce740e8
lsp <1925259616@qq.com>
2020-09-16 15:21:03 +0800

    lsp第一次登陆和注册

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -33,8 +33,15 @@
           <Verify ref="loginVerifyRef" @error="error" :showButton="false" @success="success" :width="'100%'" :height="'40px'" :fontSize="'16px'" :type="2"></Verify>
         </div>
         <div style="margin: 16px;">
-            <router-link   class="link-register" tag="span" to="./register" >立即注册</router-link>
+          <router-link   class="link-register" tag="span" to="./register" >立即注册</router-link>
           <van-button round block type="info" color="#1baeae" native-type="submit">登录</van-button>
+        </div>
+        <div class="tableTitle"><span class="midText">其他方式</span></div>
+        <div>
+          <center>
+            <van-button type="info" size="small" color="#04BE02" style="margin-left:15px; margin-top: 15px;">微信登录</van-button>
+            <van-button type="info" size="small" style="margin-left:150px;margin-top: 15px">支付宝登录</van-button>
+          </center>
         </div>
       </van-form>
     </div>
@@ -68,59 +75,68 @@
 </template>
 
 <script>
-import sHeader from '@/components/SimpleHeader'
-import { login, register, getUserInfo } from '../service/user'
-import { setLocal, getLocal } from '@/common/js/utils'
-import { Toast } from 'vant'
-import Verify from 'vue2-verify'
-export default {
-  data() {
-    return {
-      account: '',
-      password: '',
-      username1: '',
-      password1: '',
-      type: 'login',
-      verify: false
-    }
-  },
-  components: {
-    sHeader,
-    Verify
-  },
-  methods: {
-    dealTriVer() {
-      this.$refs.loginVerifyRef.$refs.instance.checkCode()
-    },
-    toggle() {
-      window.location.href = '/register'
-    },
-    async onSubmit(values) {
-      this.dealTriVer()
-      if (!this.verify) {
-        Toast.fail('验证码未填或填写错误!')
-        return
+  import sHeader from '@/components/SimpleHeader'
+  import { login, register, getUserInfo } from '../service/user'
+  import { setLocal, getLocal } from '@/common/js/utils'
+  import { Toast } from 'vant'
+  import Verify from 'vue2-verify'
+  export default {
+    data() {
+      return {
+        account: '',
+        password: '',
+        username1: '',
+        password1: '',
+        type: 'login',
+        verify: false,
+
       }
-      let data = await login({
-        "account": values.account,
-        "password": values.password
-      })
-      setLocal('token', data.token)
-      window.location.href = '/'
 
     },
-    success(obj) {
-      this.verify = true
-      // 回调之后，刷新验证码
-      obj.refresh()
+    components: {
+      sHeader,
+      Verify
     },
-    error(obj) {
-      this.verify = false
-      // 回调之后，刷新验证码
-      obj.refresh()
-    }
-  },
-}
+    methods: {
+      dealTriVer() {
+        this.$refs.loginVerifyRef.$refs.instance.checkCode()
+      },
+      toggle() {
+        window.location.href = '/register'
+      },
+      async onSubmit(values) {
+        this.dealTriVer()
+        if (!this.verify) {
+          Toast.fail('验证码未填或填写错误!')
+          return
+        }
+        let data = await login({
+          "account": values.account,
+          "password": values.password
+        })
+        setLocal('token', data.token)
+        window.location.href = '/'
+
+      },
+      success(obj) {
+        this.verify = true
+        // 回调之后，刷新验证码
+        obj.refresh()
+      },
+      error(obj) {
+        this.verify = false
+        // 回调之后，刷新验证码
+        obj.refresh()
+      },
+      // 微信授权登录  请求后端接口返回一个微信的二维码
+      wxlogin () {
+        User.wxlogins().then(res => {
+          console.log(res)
+          window.location.href = res.result.url
+        })
+      },
+    },
+  }
 </script>
 
 <style lang="less">
@@ -133,6 +149,8 @@ export default {
     }
     .login-body {
       padding: 0 20px;
+      width:50% ;
+      margin: 0 auto;
     }
     .login {
       .link-register {
@@ -188,6 +206,24 @@ export default {
           line-height: 44px;
         }
       }
+    }
+    //虚线
+    .tableTitle {
+      position: relative;
+      margin: 0 auto;
+      width: 600px;
+      height: 1px;
+      background-color: #d4d4d4;
+      text-align: center;
+      font-size: 16px;
+      color: rgba(101, 101, 101, 1);
+    }
+    .midText {
+      position: absolute;
+      left: 50%;
+      background-color:  #ffffff;
+      padding: 0 15px;
+      transform: translateX(-50%) translateY(-50%);
     }
   }
 </style>
